10ea0a56851d1e8d9d4fd9ed6960325c0eff5409
SeewonChoi <sc2179@cam.ac.uk>
2021-12-02 17:04:07 +0000

    fixed join_pp_postcode

--- fynesse/access.py
+++ fynesse/access.py
@@ -63,23 +63,24 @@ def create_index_pp(conn):
 def create_table_postcode(conn):
     cur = conn.cursor()
     cur.execute("""
-                CREATE TABLE IF NOT EXISTS `pp_data` (
-                  `transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,
-                  `price` int(10) unsigned NOT NULL,
-                  `date_of_transfer` date NOT NULL,
+                CREATE TABLE IF NOT EXISTS `postcode_data` (
                   `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
-                  `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
-                  `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
-                  `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
-                  `primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
-                  `secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
-                  `street` tinytext COLLATE utf8_bin NOT NULL,
-                  `locality` tinytext COLLATE utf8_bin NOT NULL,
-                  `town_city` tinytext COLLATE utf8_bin NOT NULL,
-                  `district` tinytext COLLATE utf8_bin NOT NULL,
-                  `county` tinytext COLLATE utf8_bin NOT NULL,
-                  `ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,
-                  `record_status` varchar(2) COLLATE utf8_bin NOT NULL,
+                  `status` enum('live','terminated') NOT NULL,
+                  `usertype` enum('small', 'large') NOT NULL,
+                  `easting` int unsigned,
+                  `northing` int unsigned,
+                  `positional_quality_indicator` int NOT NULL,
+                  `country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,
+                  `lattitude` decimal(11,8) NOT NULL,
+                  `longitude` decimal(10,8) NOT NULL,
+                  `postcode_no_space` tinytext COLLATE utf8_bin NOT NULL,
+                  `postcode_fixed_width_seven` varchar(7) COLLATE utf8_bin NOT NULL,
+                  `postcode_fixed_width_eight` varchar(8) COLLATE utf8_bin NOT NULL,
+                  `postcode_area` varchar(2) COLLATE utf8_bin NOT NULL,
+                  `postcode_district` varchar(4) COLLATE utf8_bin NOT NULL,
+                  `postcode_sector` varchar(6) COLLATE utf8_bin NOT NULL,
+                  `outcode` varchar(4) COLLATE utf8_bin NOT NULL,
+                  `incode` varchar(3)  COLLATE utf8_bin NOT NULL,
                   `db_id` bigint(20) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT
                 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
     """)
@@ -132,7 +133,7 @@ def join_pp_postcode(conn, north, south, east, west, start_date, end_date, prope
         condition = f"`property_type` = '{property_type}' AND "
     cur = conn.cursor()
     cur.execute(f"""
-              SELECT pp.price, pp.`date_of_transfer`, pp.postcode, pp.`property_type`, pp.`new_build_flag`, pp.`tenure_type`, pp.locality, pp.`town_city`, pp.district, pp.county, pp.country, pc.longitude, pc.lattitude
+              SELECT pp.price, pp.`date_of_transfer`, pp.postcode, pp.`property_type`, pp.`new_build_flag`, pp.`tenure_type`, pp.locality, pp.`town_city`, pp.district, pp.county, pc.country, pc.longitude, pc.lattitude
               FROM 
                 (SELECT lattitude, longitude, postcode FROM `postcode_data` WHERE longitude>{west} AND longitude<{east} AND lattitude>{south} AND lattitude<{north}) pc
               INNER JOIN
