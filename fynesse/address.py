8865e7dc954f0581ab22aa799cf3fbd2ddb15d3e
SeewonChoi <sc2179@cam.ac.uk>
2021-12-03 00:46:41 +0000

    prediction

--- fynesse/address.py
+++ fynesse/address.py
@@ -12,11 +12,10 @@ def choose_training_data(conn, latitude, longitude, year, property_type, box_siz
     else:
         joined_data = query_join_by_year_type(conn, latitude + box_size, latitude - box_size, longitude + box_size,
                                               longitude - box_size, year, property_type)
-
     if len(joined_data) < 1000 and box_size < 0.2:
         joined_data, box_size = choose_training_data(conn, latitude, longitude, year, property_type, box_size + 0.05)
     if len(joined_data) == 0:
-        raise ValueError("Unable to make a prediction")
+        raise ValueError("No matching training data. Unable to make a prediction")
     data = short_record_to_df(joined_data)
     return data, box_size
 
@@ -36,8 +35,10 @@ def train_model(gp_data):
 
 def predict_price(conn, latitude, longitude, year, property_type):
     pred_point = {'latitude': latitude, 'longitude': longitude}
-    training_data, box_size = choose_training_data(conn, latitude, longitude, year, property_type, 0.15)
+    training_data, box_size, data_len = choose_training_data(conn, latitude, longitude, year, property_type, 0.15)
     data = training_data.append(pred_point, ignore_index=True)
+    print("The size of the training data was " + str(len(data)))
+
     tag = [{'leisure': True}, {'sport': True}, {'healthcare': True}, {'historic': True}, {'public_transport': True},
            {'tourism': True}, {'shop': True, 'amenity': True}]
     name = ['leisure', 'sport', 'healthcare', 'historic', 'public_transport', 'tourism', 'shop_amenity']
@@ -45,9 +46,10 @@ def predict_price(conn, latitude, longitude, year, property_type):
                     longitude + box_size, longitude - box_size)
     data['ent'] = data['leisure'] + data['sport']
     data = data.fillna(0)
+
     gp_data = data[:-1]
-    data = gp_data[gp_data["price"] < gp_data["price"].quantile(0.99)]
     results_basis = train_model(gp_data)
+
     gp_point = data.iloc[-1]
     design_pred = [1, gp_point['ent'], gp_point['shop_amenity'], gp_point['healthcare'], gp_point['historic'],
                    gp_point['public_transport'], gp_point['tourism']]
